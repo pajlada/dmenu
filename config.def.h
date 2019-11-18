886db1a3eb847ce98cbca9a99219270f1a358e13
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-11-18 13:48:29 +0100

    Update default font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 0;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"PragmataPro:pixelsize=16:antialias=true:autohint=false"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
